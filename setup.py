7dfef09173627c508da3332c9ec40c81682ecd3d
Peter Boyer <petertboyer@gmail.com>
2017-07-12 17:46:12 +1000

    Bump v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='backplate-auth',
-    version='0.0.1',
+    version='0.0.2',
     description='RESTful API Auth Helpers',
     author='Peter Boyer',
     author_email='petertboyer@gmail.com',
